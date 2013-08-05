cb051059e1f4d7d8c6c8b4f4844cd4269c934c73
7stud <7stud@excite.com>
2013-08-05 04:58:52 -0600

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,8 @@
 module ApplicationHelper
 
-=begin
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
-=end
 
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
     else
